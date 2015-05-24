2cec2c969c7ce344dc1a6926997db9ecfc1f1955
JitNut <jithil@outlook.com>
2015-05-24 14:47:30 +0530

    iCHART implementation

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
+angular.module('smartHome', ['ionic','n3-line-chart'])
 .controller('roomsDataCtrl',function($scope,$http,$window){
     
-    window.addEventListener("orientationchange", function() {
-   alert(window.orientation);
-});
+    screen.orientation.onchange = function() {
+        alert('rotated');
+    };
     var graphData = {
         'Line' : [],
         'Pie' :  []
@@ -19,12 +19,17 @@ angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
     };
     
     function initGraphData(){
-     $http.get('graphData.line.json').success(function(data) {
-         graphData.Line  = data;
-         });
+     
          $http.get('graphData.pie.json').success(function(data) {
          graphData.Pie  = data;
          });
+        $http.get('roomsData.json').success(function(data) {
+        $scope.rooms = data;
+        });
+        $http.get('graphOptions.line.json').success(function(data) {
+         $scope.graphOptions = data;
+            $scope.graphOptions.axes.x.labelFunction = function(value) {return value;}
+        });
     }
     
      $scope.xFunction = function() {
@@ -48,57 +53,6 @@ angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
     }
     
     $scope.graphData =[];
-   $scope.graphOptions = {  
-  series: [
-    {
-      y: "vrms",
-      label: "vrms",
-      type: "area",
-      color: "#4EC213",
-      axis: "y",
-      thickness: "1px",
-      dotSize: 2,
-      striped :true,
-       drawLegend: true,
-      id: "series_0"
-    },
-    {
-      y: "vrmsAvg",
-      label: "vrmsAvg",
-      type: "line",
-      color: "#0B9E7C",
-      axis: "y1",
-      striped :true,
-      dotSize: 3,
-        drawLegend: true,
-      id: "series_1"
-    },
-    {
-      y: "irms",
-      label: "irms",
-      color: "#9467bd",
-      axis: "y1",
-      type: "line",
-      thickness: "1px",
-      dotSize: 3,
-        drawLegend: true,
-      id: "series_2"
-    }
-  ],
-  stacks: [],
-  axes: {
-    x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', min: 0, max: 10, ticks: 10},
-    y: {type: 'linear', min: 114, max: 125, ticks: 10},
-    y2: {type: 'linear', min: 114, max: 125, ticks: 10},
-    y3: {type: 'linear', min: 0, max: 125, ticks: 10}
-  },
-  tension: 0.2,
-  tooltip: {mode: "scrubber"},
-  drawLegend: true,
-  drawDots: true,
-  columnsHGap: 2,
-  lineMode: "dashed"
-};
     
     $scope.Views = {'HomeView' : 'HomeView',
                     'RoomView': 'RoomView',
@@ -136,20 +90,6 @@ angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
                  ]
         }
     ];
-    
-    $scope.rooms = [{'Heading' : "Living Room", 'img':'img/living_room.png', 'id':'sm001'},
-                   {'Heading' : "Bath Room", 'img':'img/bath_room.png', 'id':'sm002'},
-                   {'Heading' : "Dinning Room", 'img':'img/dining_room.png', 'id':'sm003'},
-                   {'Heading' : "Bed Room", 'img':'img/sleeping_room.png', 'id':'sm004'},
-                    {'Heading' : "Living Room", 'img':'img/living_room.png', 'id':'sm001'},
-                   {'Heading' : "Bath Room", 'img':'img/bath_room.png', 'id':'sm002'},
-                   {'Heading' : "Dinning Room", 'img':'img/dining_room.png', 'id':'sm003'},
-                   {'Heading' : "Bed Room", 'img':'img/sleeping_room.png', 'id':'sm004'},
-                    {'Heading' : "Living Room", 'img':'img/living_room.png', 'id':'sm001'},
-                   {'Heading' : "Bath Room", 'img':'img/bath_room.png', 'id':'sm002'},
-                   {'Heading' : "Dinning Room", 'img':'img/dining_room.png', 'id':'sm003'},
-                   {'Heading' : "Bed Room", 'img':'img/sleeping_room.png', 'id':'sm004'}
-                   ];
     
     
     
@@ -192,10 +132,15 @@ angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
                 case chartTypes.Pie:
                 $scope.graphData = graphData.Pie;
                 $scope.isLineChart = false;
-                //$scope.graphOptions =
+                drawPieChart();
                 break;
         }
         setView($scope.Views.ChartView);
+    }
+    
+    function drawPieChart()
+    {
+       
     }
    function getDescriptionFromId(id){
        for(i=0;i<$scope.rooms.length;i++){
@@ -233,6 +178,123 @@ angular.module('smartHome', ['ionic','n3-line-chart','nvd3ChartDirectives'])
     initGraphData();
     
     $scope.setView = setView;
+    
+    $scope.drawPieChart = function (div){
+        new iChart.Pie2D({
+					render : div,
+					data: $scope.graphData,
+
+					legend : {
+						enable : false,
+						align : 'center',
+						sign : 'round',
+						row:'max',
+						
+					},
+					label : {
+						sign: 'round'		
+					},
+            align : 'center',
+					showpercent:true,
+					decimalsnum:2,
+					layout_distance : 1,
+					border : true,
+					tip  : 'enable'
+				}).draw();
+    };
+    
+    $scope.drawLineChart = function(div){
+        document.body.style.overflow = "hidden";
+
+				var vrms = [125.0,124.829629132,124.330127019,123.535533906,122.5,121.294095226,120.0,118.705904775,117.5,116.464466095,115.669872982,115.170370869,115.0,115.170370869,115.669872982,116.464466095,117.5,118.705904775,120.0,121.294095226,122.5,123.535533906,124.330127019,124.829629132,125.0];
+				var vrmsAvg = [120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0,120.0];
+
+
+				var timestamp  ={0:4.16667E-4,1:0.042083334,2:0.08375,3:0.125416667,4:0.167083334,5:0.20875,6:0.250416667,7:0.292083334,8:0.33375,9:0.375416667,10:0.417083334,11:0.458750001,12:0.500416667,13:0.542083334,14:0.58375,15:0.625416667,16:0.667083334,17:0.70875,18:0.750416667,19:0.792083334,20:0.83375,21:0.875416667,22:0.917083334,23:0.95875,24:1.000416667};
+				var data = [
+				         	{
+				         		name : 'VRMS',
+				         		value:vrms,
+				         		color:'#aad0db',
+				         		line_width:2
+				         	},
+				         	{
+				         		name : 'VRMS Avg',
+				         		value:vrmsAvg,
+				         		color:'#f68f70',
+				         		line_width:2
+				         	}
+				         ];
+
+				var chart = new iChart.LineBasic2D({
+					render : div,
+					data:data,
+                    width: $scope.expliciteWidth - 20,
+					align:'left',
+                    padding : 30,
+					background_color:'#FEFEFE',
+					border: false,
+					tip:{
+						enable:true,
+						shadow:true,
+						move_duration:400,
+						border:{
+							 enable:true,
+							 radius : 5,
+							 width:2,
+							 color:'#3f8695'
+						},
+						listeners:{
+							 //tip:
+							parseText:function(tip,name,value,text,i){
+								return name+":"+value;
+							}
+						}
+					},
+					tipMocker:function(tips,i){
+						return "<div style='font-weight:600'> Time : "+timestamp[i]+
+								"</div>"+tips.join("<br/>");
+					},
+					legend : {
+						enable : true,
+						row:1,
+						column : 'max',
+						valign:'top',
+						sign:'bar',
+						background_color:null,
+						offsetx:-80,
+						border : true
+					},
+					crosshair:{
+						enable:true,
+						line_color:'#62bce9'
+					},
+					sub_option : {
+						label:false,
+						point_size:10
+					},
+					coordinate:{
+						width:$scope.expliciteWidth ,
+						height:$scope.expliciteHeight ,
+						axis:{
+							color:'#dcdcdc',
+							width:1
+						},
+						scale:[{
+							 position:'left',	
+							 start_scale:114,
+							 end_scale:125,
+							 scale_space:10,
+							 scale_size:2,
+							 scale_color:'#9f9f9f'
+						},{
+							 position:'bottom',	
+							 labels:"0"
+						}]
+					}
+				});
+			chart.draw();
+    }
 })
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
